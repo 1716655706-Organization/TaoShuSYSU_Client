b8d5d554496db35802682369fe08843ed1c4a86e
kuxinwei <kuxinwei@163.com>
2014-06-05 13:39:28 +0800

    Signed-off-by: kuxinwei <kuxinwei@163.com>

--- src/com/sysu/taosysu/network/NetworkRequest.java
+++ src/com/sysu/taosysu/network/NetworkRequest.java
@@ -1,31 +1,30 @@
 package com.sysu.taosysu.network;
 
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import android.R.integer;
-import android.os.AsyncTask;
-
-
 public class NetworkRequest {
-	
+
 	public static HttpClient CLIENT = new DefaultHttpClient();
 	private static String SERVER_URI = "http://192.168.1.1/TaoshuSYSU/server.php";
-	
-	public static void register(String userName, String password, RegisterAsyncTask.OnRequestListener listener) {
+
+	public static void register(String userName, String password,
+			RegisterAsyncTask.OnRequestListener listener) {
 		new RegisterAsyncTask(userName, password, listener).execute(SERVER_URI);
 	}
 
-	public static void login(String userName, String password, LoginAsyncTask.OnRequestListener listener) {
+	public static void login(String userName, String password,
+			LoginAsyncTask.OnRequestListener listener) {
 		new LoginAsyncTask(userName, password, listener).execute(SERVER_URI);
 	}
-	
-	public static void uploadBook(int userId, String bookName, String content, String[] label, UploadBookAsyncTask.OnRequestListener listener) {
-		new UploadBookAsyncTask(userId, bookName, content, label, listener).execute(SERVER_URI);
+
+	public static void uploadBook(int userId, String bookName, String content,
+			String[] label, UploadBookAsyncTask.OnRequestListener listener) {
+		new UploadBookAsyncTask(userId, bookName, content, label, listener)
+				.execute(SERVER_URI);
 	}
-	
+
 	public static void getBookList(int startBookId, int Size) {
-		
+
 	}
 }
